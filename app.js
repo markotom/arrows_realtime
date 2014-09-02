83254fbd30887239f06031b95fd562d7a8bae497
Marco Godínez <markotom@gmail.com>
2014-09-02 18:49:09 -0500

    Listen on env port if exists

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express'),
     app     = express(),
-    server  = app.listen(3000),
+    server  = app.listen(process.env.PORT || 3000),
     socket  = require('socket.io'),
     io      = socket.listen(server);
 
